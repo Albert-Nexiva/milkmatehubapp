53e5f2aaadd62cde3e391a56b6647e41234a19cf
Abhin S <abhin.s@optimetech.com>
2024-05-05 14:51:21 +0530

    fix: scroll

--- lib/admin_module/screens/production_screen.dart
+++ lib/admin_module/screens/production_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:intl/intl.dart';
 import 'package:milkmatehub/constants/methods.dart';
 import 'package:milkmatehub/firebase/firestore_db.dart';
@@ -25,29 +26,30 @@ class ProductionsScreen extends StatelessWidget {
             if (items!.isNotEmpty) {
               return Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Container(
-                        width: double.infinity,
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: generateRandomColor().withOpacity(0.8),
-                          border: Border.all(
-                              color: generateRandomColor(), width: 1),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Text(
-                          "Total production: ${items.fold<double>(0, (sum, record) => sum + double.parse(record.liter))} Liters",
-                          style: const TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: generateRandomColor().withOpacity(0.8),
+                        border:
+                            Border.all(color: generateRandomColor(), width: 1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        "Total production: ${items.fold<double>(0, (sum, record) => sum + double.parse(record.liter))} Liters",
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
                       ),
-                      const SizedBox(height: 10),
-                      ListView.builder(
+                    ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: ListView.builder(
+                        physics: const AlwaysScrollableScrollPhysics(),
                         shrinkWrap: true,
                         itemCount: items.length,
                         itemBuilder: (context, index) {
@@ -112,8 +114,8 @@ class ProductionsScreen extends StatelessWidget {
                           );
                         },
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               );
             } else {
