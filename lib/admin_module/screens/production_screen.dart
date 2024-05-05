3569f59118e9dd107d2a26781edf102d209bc1fa
Abhin S <abhin.s@optimetech.com>
2024-05-05 14:07:46 +0530

    fix: scroll issue

--- lib/admin_module/screens/production_screen.dart
+++ lib/admin_module/screens/production_screen.dart
@@ -25,91 +25,95 @@ class ProductionsScreen extends StatelessWidget {
             if (items!.isNotEmpty) {
               return Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      padding: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        color: generateRandomColor().withOpacity(0.8),
-                        border:
-                            Border.all(color: generateRandomColor(), width: 1),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Text(
-                        "Total production: ${items.fold<double>(0, (sum, record) => sum + double.parse(record.liter))} Liters",
-                        style: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: generateRandomColor().withOpacity(0.8),
+                          border: Border.all(
+                              color: generateRandomColor(), width: 1),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Text(
+                          "Total production: ${items.fold<double>(0, (sum, record) => sum + double.parse(record.liter))} Liters",
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: items.length,
-                      itemBuilder: (context, index) {
-                        final record = items[index];
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.grey, width: 1),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: ListTile(
-                              leading: const Icon(Icons.poll_rounded, size: 38),
-                              title: Column(
-                                children: [
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text('Supplier: ${record.supplierName}',
-                                          style: const TextStyle(
-                                              fontWeight: FontWeight.bold)),
-                                      // Text(
-                                      //   'Date: ${DateFormat('dd-MM-yyyy').format(DateTime.parse(record.date))}',
-                                      //   style: const TextStyle(
-                                      //       fontWeight: FontWeight.bold),
-                                      // ),
-                                    ],
-                                  ),
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text('Liter: ${record.liter}',
-                                          style: const TextStyle(
-                                              fontWeight: FontWeight.bold)),
-                                      Text(
-                                        'Date: ${DateFormat('dd-MM-yyyy').format(DateTime.parse(record.date))}',
-                                        style: const TextStyle(
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                    ],
-                                  ),
-                                ],
+                      const SizedBox(height: 10),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: items.length,
+                        itemBuilder: (context, index) {
+                          final record = items[index];
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.grey, width: 1),
+                                borderRadius: BorderRadius.circular(10),
                               ),
-                              subtitle: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text('FAT: ${record.fat}'),
-                                  const SizedBox(width: 10),
-                                  Text('SNF: ${record.snf}'),
-                                  const SizedBox(width: 10),
-                                  Text('CLR: ${record.clr}'),
-                                ],
+                              child: ListTile(
+                                leading:
+                                    const Icon(Icons.poll_rounded, size: 38),
+                                title: Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text('Supplier: ${record.supplierName}',
+                                            style: const TextStyle(
+                                                fontWeight: FontWeight.bold)),
+                                        // Text(
+                                        //   'Date: ${DateFormat('dd-MM-yyyy').format(DateTime.parse(record.date))}',
+                                        //   style: const TextStyle(
+                                        //       fontWeight: FontWeight.bold),
+                                        // ),
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text('Liter: ${record.liter}',
+                                            style: const TextStyle(
+                                                fontWeight: FontWeight.bold)),
+                                        Text(
+                                          'Date: ${DateFormat('dd-MM-yyyy').format(DateTime.parse(record.date))}',
+                                          style: const TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                subtitle: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text('FAT: ${record.fat}'),
+                                    const SizedBox(width: 10),
+                                    Text('SNF: ${record.snf}'),
+                                    const SizedBox(width: 10),
+                                    Text('CLR: ${record.clr}'),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                      },
-                    ),
-                  ],
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               );
             } else {
